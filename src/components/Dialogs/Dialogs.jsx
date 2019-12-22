883cc8b43b82a3ae03160902cad233a5bda51944
ArtemSpiridovich <Arte.spiridovich99@gmail.com>
2019-12-22 11:50:40 +0300

    lesson-24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,21 +4,35 @@ import Dialog from "./Dialog/Dialog";
 import Message from "./Message/Message";
 
 const Dialogs = () => {
+    let dialogsData = [
+        {id:1, name: 'Dimych'},
+        {id:2, name: 'Andrey'},
+        {id:3, name: 'Svetka'},
+        {id:4, name: 'Diana'},
+        {id:5, name: 'Victor'},
+        {id:6, name: 'Valera'}
+    ];
+    let messagesData = [
+        {mesage: 'Hi'},
+        {mesage: 'How are you?'},
+        {mesage: 'What do you?'},
+        {mesage: 'Yoooooooooo'}
+    ];
     return (
             <div className={s.dialogs}>
                 <div className={s.dialogsitems}>
-                    <Dialog name="Dimych" id='1'/>
-                    <Dialog name="Andrey" id='2'/>
-                    <Dialog name="Svetka" id='3'/>
-                    <Dialog name="Diana" id='4'/>
-                    <Dialog name="Victor" id='5'/>
-                    <Dialog name="Valera" id='6'/>
+                    <Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+                    <Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+                    <Dialog name={dialogsData[2].name} id={dialogsData[2].id}/>
+                    <Dialog name={dialogsData[3].name} id={dialogsData[3].id}/>
+                    <Dialog name={dialogsData[4].name} id={dialogsData[4].id}/>
+                    <Dialog name={dialogsData[5].name} id={dialogsData[5].id}/>
                 </div>
                 <div className={s.messages}>
-                    <Message message='Hi'/>
-                    <Message message='How are you?'/>
-                    <Message message='what do you?'/>
-                    <Message message='Yo'/>
+                    <Message message={messagesData[0].mesage}/>
+                    <Message message={messagesData[1].mesage}/>
+                    <Message message={messagesData[2].mesage}/>
+                    <Message message={messagesData[3].mesage}/>
                 </div>
             </div>
     );
