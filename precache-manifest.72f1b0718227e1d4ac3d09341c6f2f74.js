38d4b8ad471ee63e09ee09d23d3e766a39c6d4d0
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-02-24 11:49:18 +0300

    Updates

---
+++ precache-manifest.72f1b0718227e1d4ac3d09341c6f2f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c33af67a7382c1e2a3f3e91bcc01d7",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "d64592109c8ff673f6f4",
+    "url": "/social-network/static/css/main.3fbe406b.chunk.css"
+  },
+  {
+    "revision": "f45567b20e30f0c19701",
+    "url": "/social-network/static/js/2.794a7ed6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/social-network/static/js/2.794a7ed6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d64592109c8ff673f6f4",
+    "url": "/social-network/static/js/main.76512ab3.chunk.js"
+  },
+  {
+    "revision": "b4e47c17a0c124211064",
+    "url": "/social-network/static/js/runtime-main.0a7301e8.js"
+  },
+  {
+    "revision": "8f2e5470cb4ec34beed6d1fa95c18e20",
+    "url": "/social-network/static/media/Blocks-1s-200px.8f2e5470.svg"
+  }
+]);
